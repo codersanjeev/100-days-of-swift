77d9544ce19a757eb20e236b761e3ebbe3e8d2b3
Sanjeev <thedevelopersanjeev@gmail.com>
2021-02-22 19:21:22 +0530

    completed day one

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -12,3 +12,33 @@ let colors = Set(["red", "green", "blue", "red", "blue"])
 print(colors)
 
 // Tuples: 
+
+class DemoInt {
+	var val: Int = 0
+	
+	init(_ val: Int) {
+		self.val = val
+	}
+	
+}
+var data: [[DemoInt]] = []
+
+data.append([DemoInt(1), DemoInt(2), DemoInt(3)])
+data.append([DemoInt(4), DemoInt(5), DemoInt(6)])
+
+func getData() -> [DemoInt] {
+	return data[0]
+}
+
+func modifyData() {
+	var tempData = getData()
+	tempData[1].val = 100
+}
+
+func printData() {
+	data.forEach { $0.forEach { print($0.val) } }
+}
+	
+printData()
+modifyData()
+printData()
