d3bf3fea1d32e2ec88a4ca84119edcdc680c27d4
Sanjeev <sanjeev.sharma@nagarro.com>
2021-04-15 10:57:15 +0530

    completed project two

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -1,1 +1,26 @@
+class Parent {
+	var name: String
+	
+	init(name: String) {
+		self.name = name
+	}
+}
 
+class Child : Parent {
+	override init(name: String) {
+		super.init(name: name)
+		self.name = name
+	}
+}
+
+func functionOne<T : Parent>(object: T) {
+	print(object.name)
+}
+
+func functionTwo(object: Parent) {
+	print(object.name)
+}
+
+let object = Child(name: "object name")
+functionOne(object: object)
+functionTwo(object: object)
