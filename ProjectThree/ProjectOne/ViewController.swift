a052f6bd7fff2e27c47e4d7590376f238f5a8420
Sanjeev <sanjeev.sharma@nagarro.com>
2021-04-15 11:09:21 +0530

    completed project three

---
+++ ProjectThree/ProjectOne/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  ProjectOne
+//
+//  Created by sanjeev on 31/03/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+	private var pictures = [String]()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let fileManager = FileManager.default
+		guard let path = Bundle.main.resourcePath else { return }
+		let items = try! fileManager.contentsOfDirectory(atPath: path)
+		items.forEach { item in
+			if (item.hasPrefix("nssl")) {
+				pictures.append(item)
+			}
+		}
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return pictures.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+		cell.textLabel?.text = pictures[indexPath.row]
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if let detailsViewController = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+			detailsViewController.selectedImage = pictures[indexPath.row]
+			navigationController?.pushViewController(detailsViewController, animated: true)
+		}
+	}
+}
